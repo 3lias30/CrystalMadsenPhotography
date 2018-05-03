8ddc4d87f243efeb0868bd887448b09f3d654d87
Braden <Braden@my.innovationspokane.org>
2018-05-03 11:24:32 -0700

    Auto stash before merge of "master" and "origin/master"
    Added a collection view controler for the favorites page and should be ready for code. Also resized a few of the cell sizes on the first few pages.

---
+++ Tester2/FavoritesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FavoritesCollectionViewCell.swift
+//  Tester2
+//
+//  Created by CS Student on 5/3/18.
+//  Copyright © 2018 CS Student. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var FavImageView: UIImageView!
+    
+}
---
+++ Tester2/favoriteCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  favoriteCollectionViewCell.swift
+//  Tester2
+//
+//  Created by CS Student on 5/3/18.
+//  Copyright © 2018 CS Student. All rights reserved.
+//
+
+import UIKit
+
+class favoriteCollectionViewCell: UICollectionViewCell {
+    
+}
